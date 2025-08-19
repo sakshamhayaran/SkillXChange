99bc88931382d5b7a03e97eb2ad89e0bb2492cb8
Saksham Hayaran <hsaksham0408@gmail.com>
2025-08-19 15:33:48 +0530

    Deploying to Github pages

--- src/main.jsx
+++ src/main.jsx
@@ -13,51 +13,54 @@ import Home from './pages/Home.jsx'
 import UserProfile from './pages/UserProfile.jsx'
 
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />
+    },
+    {
+      path: "/register_learner",
+      element: <Register_Learner />
+    },
+    {
+      path: "/register_tutor",
+      element: <Register_Tutor />
+    },
+    {
+      path: "/login_learner",
+      element: <Login_Learner />
+    },
+    {
+      path: "/login_tutor",
+      element: <Login_Tutor />
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: "profile",
+          element: <Profile />
+        },
+        {
+          path: "users/:id",
+          element: <UserProfile />
+        }
+      ]
+    }
+  ],
   {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/register_learner",
-    element: <Register_Learner />
-  },
-  {
-    path: "/register_tutor",
-    element: <Register_Tutor />
-  },
-  {
-    path: "/login_learner",
-    element: <Login_Learner />
-  },
-  {
-    path: "/login_tutor",
-    element: <Login_Tutor />
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "profile",
-        element: <Profile />
-      },
-      {
-        path: "users/:id",
-        element: <UserProfile />
-      },
-    ]
+    basename: '/SkillXChange'
   }
-])
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} >
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
