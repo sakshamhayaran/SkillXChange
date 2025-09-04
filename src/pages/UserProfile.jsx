2f6a74e602da75db54ba6f1cfbcd2e9d5a7740ea
Saksham Hayaran <hsaksham0408@gmail.com>
2025-09-04 23:33:33 +0530

    Update Request functionality to learner

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,6 +1,6 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { addDoc, collection, doc, getDoc, getDocs, query, where, serverTimestamp } from 'firebase/firestore';
 import { useEffect, useState } from 'react'
-import { db } from '../Firebase/firebase';
+import { auth, db } from '../Firebase/firebase';
 import { useParams } from 'react-router-dom';
 
 function UserProfile() {
@@ -23,6 +23,33 @@ function UserProfile() {
         fetchUser();
     }, [role, id]);
 
+    const sendRequest = async () => {
+        try {
+            const user = auth.currentUser;
+            if (!user) { alert("⚠️ You must be logged in to send a request."); return; }
+
+            const existing = await getDocs(query(collection(db, "requests"),
+                where("senderId", "==", user.uid),
+                where("receiverId", "==", id)
+            ));
+            if(existing.empty == false) { alert("❗You already sent a request to this tutor !"); return; }
+
+            const requestObj = await addDoc(collection(db, "requests"), {
+                senderId: user.uid,
+                receiverId: id,
+                type: "learner-request",
+                status: "pending",
+                timestamp: serverTimestamp(),
+            });
+            console.log(requestObj)
+            alert("✅ Request sent successfully !");
+        }
+        catch (error) {
+            console.error("!!Error sending request:", error.message);
+            alert("❌ Could not send request. Please try again.");
+        }
+    }
+
     if (!userData) { return (<p className="text-center p-5">Loading....</p>) }
 
     return (
@@ -35,6 +62,8 @@ function UserProfile() {
             <div className="w-full md:w-[1/2] flex flex-col gap-5 p-10">
                 <div className="flex justify-evenly items-center">
                     <h1 className="md:text-4xl text-2xl font-bold text-center">User Profile !</h1>
+                    {role === 'learners' &&
+                        (<button onClick={sendRequest} className="bg-blue-500 text-white px-4 py-2 rounded">Request</button>)}
                 </div>
                 <p className="text-md"><span className="font-bold">Full Name : </span>{userData.fullName}</p>
                 <p className="text-md"><span className="font-bold">Email : </span>{userData.email}</p>
