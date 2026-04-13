ca79a6421072fd0e495ee66c248ce1b2561aa642
Saksham Hayaran <hsaksham0408@gmail.com>
2026-04-13 15:12:34 +0530

    Fixed Bugs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  base: '/SkillXChange_with_Firebase/',
+  base: '/SkillXChange/',
   plugins: [react()],
 }));
